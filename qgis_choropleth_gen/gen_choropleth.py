f0c8d587710b2846fbaf57b77e6e4957f4b26697
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-01-17 11:43:55 +0100

    Fully functional spectral choropleth.

--- qgis_choropleth_gen/gen_choropleth.py
+++ qgis_choropleth_gen/gen_choropleth.py
@@ -1,15 +1,15 @@
-bottom = 0
-top = 100
-num_classes = 7
+bottom = 21.1
+top = 37.5
+num_classes = 50
 colours = [[215, 25, 28], [254, 238, 171], [43, 131, 186]]
+
 
 increment = (top - bottom) / num_classes
 col_levels = num_classes / (len(colours) - 1) 
 
 col_increments = []
 
-header = """
-<?xml version="1.0" encoding="UTF-8"?>
+header = """<?xml version="1.0" encoding="UTF-8"?>
 <StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.1.0" xmlns:xlink="http://www.w3.org/1999/xlink" units="mm" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.1.0/StyledLayerDescriptor.xsd" xmlns:se="http://www.opengis.net/se">
   <NamedLayer>
     <se:Name>test8DEMFinal.hasc output Polygon</se:Name>
@@ -52,7 +52,7 @@ def create_level(bot, top, colour):
             </se:Fill>
             <se:Stroke>
               <se:SvgParameter name="stroke">#""" + colour + """</se:SvgParameter>
-              <se:SvgParameter name="stroke-width">1</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0</se:SvgParameter>
               <se:SvgParameter name="stroke-linejoin">bevel</se:SvgParameter>
             </se:Stroke>
           </se:PolygonSymbolizer>
@@ -78,14 +78,17 @@ for i in range(num_classes):
     
     num_inc = i % col_levels
     
-    r = hex(int(colours[i_col][0] + num_inc * col_increments[i_col][0])).split('x')[1]    
-    g = hex(int(colours[i_col][1] + num_inc * col_increments[i_col][1])).split('x')[1]
-    b = hex(int(colours[i_col][2] + num_inc * col_increments[i_col][2])).split('x')[1]
+    try:
+        r = hex(int(colours[i_col][0] + num_inc * col_increments[i_col][0])).split('x')[1]    
+        g = hex(int(colours[i_col][1] + num_inc * col_increments[i_col][1])).split('x')[1]
+        b = hex(int(colours[i_col][2] + num_inc * col_increments[i_col][2])).split('x')[1]
+    except Exception:
+        print ("Exception: " + str(i_col) + " " + str(num_inc))
     
     val_bot = bottom + i * increment 
     val_top = val_bot + increment
     
-    i_col = int(i / len(colours))
+    i_col = int((i + 1) / col_levels)
     
     print (create_level(val_bot, val_top, r + g + b))
     
